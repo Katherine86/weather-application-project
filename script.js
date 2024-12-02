796407ec0d9482d57731ebeff9f5290bb2be49d2
Katherine86 <katherine_young_2010@yahoo.co.uk>
2024-12-02 18:57:03 +0000

    Created temperature function

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+function updateWeatherData(response) {
+  let temperatureElement = document.querySelector("#weather-app-temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "ff7aoa188320d523t26fbb340bccff49";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeatherData);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#weather-app-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
