3decca98b07a50e11ce2a3dc9d4b639477ca9f48
Katherine86 <katherine_young_2010@yahoo.co.uk>
2024-12-08 10:14:00 +0000

    Search functionality using API

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function updateWeatherData(response) {
   let temperatureElement = document.querySelector("#weather-app-temperature");
   let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-app-city");
+
+  cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
@@ -14,10 +17,11 @@ function handleSearchSubmit(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Sunderland");
